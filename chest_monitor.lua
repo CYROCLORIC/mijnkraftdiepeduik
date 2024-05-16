15424460e50e9cf773a3a827008dec020cd3ed83
Yaseen Javed <153917935+CYROCLORIC@users.noreply.github.com>
2024-05-16 17:14:19 +0200

    Add files via upload

---
+++ chest_monitor.lua
@@ -0,0 +1,23 @@
+local chest = peripheral.find("minecraft:chest")
+local monitor = peripheral.find("monitor")
+
+
+local width, height = monitor.getSize()
+local Y = math.floor((height - 2 * #chest.list()) / 2)
+
+for slot, item in pairs(chest.list()) do
+    local itemDetail = chest.getItemDetail(slot)
+    if itemDetail ~= nil then
+
+        local xPos = math.floor((width - #itemDetail.displayName) / 2)
+        monitor.setCursorPos(xPos, Y)
+        monitor.write(itemDetail.displayName)
+
+
+        local quantityPos = math.floor((width - #tostring(itemDetail.count)) / 2)
+        monitor.setCursorPos(quantityPos, Y + 1)
+        monitor.write(tostring(itemDetail.count))
+
+        Y = Y + 2
+    end
+end`
